f9fdfa41e2064a85e2b01200eb6700cb1e970e14
Pavan Kanakkassery <145629624+pjk132020@users.noreply.github.com>
2024-11-12 14:28:38 -0500

    Fixed Errors on the flappy bird

--- stepperMotorRotatingGameANDBird.ino
+++ stepperMotorRotatingGameANDBird.ino
@@ -60,13 +60,13 @@ class StepperControl {
 };
 
 // Instantiate StepperControl for Motor 2 with specific motor settings
-StepperControl motor2(motorInterfaceType, 22, 23, 24, 25, 3000, 500, 3000, 12000, 500);  // 500 ms flap duration
+StepperControl motor2(motorInterfaceType, 22, 23, 24, 25, 1000, 500, 3000, 12000, 500);  // 500 ms flap duration
 
 void setup() {
   // Initialize Motor 1 settings
   motor1.setMaxSpeed(1000); // Maximum steps per second
-  motor1.setAcceleration(500); // Acceleration in steps per second^2
-  int targetRPM1 = 3500;  // Set your target RPM for Motor 1
+  motor1.setAcceleration(200); // Acceleration in steps per second^2
+  int targetRPM1 = 1000;  // Set your target RPM for Motor 1
   float stepsPerSecond1 = (targetRPM1 * stepsPerRevolution) / 60.0;
   motor1.setSpeed(stepsPerSecond1);
 
@@ -83,6 +83,8 @@ void loop() {
   if (digitalRead(buttonPin) == LOW) {
     motor2.applyFlap();  // Apply a counterclockwise "flap" for Motor 2
   }
-
+  else if(digitalRead(buttonPin) == HIGH){
   motor2.update();  // Update Motor 2 direction based on flap duration
   motor2.run();  // Run Motor 2 at the set speed
+  }
+}
